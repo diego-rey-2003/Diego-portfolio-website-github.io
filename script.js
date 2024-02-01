22e8c54db937c93438d16c97fc09106c09e41fe1
diego-rey-2003 <144565295+diego-rey-2003@users.noreply.github.com>
2024-02-01 09:48:37 -0800

    Final product

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglemenu()
+{
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".champions-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open")
+
+}
